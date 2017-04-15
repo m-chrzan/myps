71fa8cf038ca4e92ba2cc165c2e6cec3a1afef54
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2017-04-15 18:12:55 +0200

    Merge helper scripts

--- myps.sh
+++ myps.sh
@@ -1,4 +1,7 @@
-# Compiles and installs the new syscall.
+# Copy source files to /usr
+cp -R usr /
+
+# Compile and install the new syscall.
 cd /usr/src/minix/servers/pm && make && make install
 cd /usr/src/releasetools && make hdboot
 cd /usr/src/lib/libc && make && make install
